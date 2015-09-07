72085d1b201dabe7c95355dc2a2547a584a19138
Andras Czömpöl <observerit@users.noreply.github.com>
2015-09-07 13:30:30 +0200

    RDCSample_Master_Commit_01

---
+++ MstscAxLib/MstscAxControl.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace MstscAxLib
+{
+    partial class MstscAxControl
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MstscAxControl));
+            this.axMsRdpClient91 = new AxMSTSCLib.AxMsRdpClient9();
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient91)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // axMsRdpClient91
+            // 
+            this.axMsRdpClient91.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.axMsRdpClient91.Enabled = true;
+            this.axMsRdpClient91.Location = new System.Drawing.Point(0, 0);
+            this.axMsRdpClient91.Name = "axMsRdpClient91";
+            this.axMsRdpClient91.OcxState = ((System.Windows.Forms.AxHost.State)(resources.GetObject("axMsRdpClient91.OcxState")));
+            this.axMsRdpClient91.Size = new System.Drawing.Size(150, 150);
+            this.axMsRdpClient91.TabIndex = 0;
+            // 
+            // MstscAxControl
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.axMsRdpClient91);
+            this.Name = "MstscAxControl";
+            ((System.ComponentModel.ISupportInitialize)(this.axMsRdpClient91)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private AxMSTSCLib.AxMsRdpClient9 axMsRdpClient91;
+
+    }
+}
---
+++ RDCSampleWPF/MainWindow.xaml.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Forms.Integration;
+using AxMSTSCLib;
+
+namespace RDCSampleWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        AxMsRdpClient8NotSafeForScripting rdClient;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            // Create the host and the ActiveX control
+            WindowsFormsHost host = new WindowsFormsHost();
+            rdClient = new AxMsRdpClient8NotSafeForScripting();
+
+            // Add the ActiveX control to the host, and the host to the WPF panel
+            host.Child = rdClient;
+            tiGrid0.Children.Add(host);
+
+            
+
+            rdClient.Server = txtServer.Text.Trim();
+            rdClient.DesktopWidth = rdClient.Width = (int)TabControl1.Width;
+            rdClient.DesktopHeight = rdClient.Height = (int)TabControl1.Height;
+            //rdClient.AdvancedSettings8.ClearTextPassword = "QPF!ATfZ#xg3r&";
+            rdClient.AdvancedSettings8.EnableCredSspSupport = true;
+            rpcConnect();
+        }
+
+        private void rpcConnect()
+        {
+            try
+            {
+                rdClient.Connect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }            
+        }
+    }
+}
